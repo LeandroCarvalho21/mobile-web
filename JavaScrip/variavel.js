8b994def1b6d0fa9d72f66e500977d8e6e84620c
Leandro Carvalho21 <leandrocnovais@gmail.com>
2025-09-01 15:53:33 -0300

    Adicionando o arquivo variavel.js

---
+++ JavaScrip/variavel.js
@@ -0,0 +1,16 @@
+let mensagem = "Olá mundo ";
+
+const nomeCompleto = "Leandro Carvalho ";
+
+let idade = 32;
+
+//Resultado
+console.log(mensagem);
+console.log(nomeCompleto);
+console.log("Meu nome é: " + nomeCompleto + "e a idade é: " + idade + " Anos");
+
+
+
+
+
+
