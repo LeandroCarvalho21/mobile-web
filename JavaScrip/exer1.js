6f7134ad493c3018a0c1ebdb27b14be82cbb9809
Leandro Carvalho21 <leandrocnovais@gmail.com>
2025-09-01 16:21:40 -0300

    Adicionando o exer1.js

---
+++ JavaScrip/exer1.js
@@ -0,0 +1,23 @@
+// Variavel com meu nome completo
+let nome = "Leandro Carvalho";
+
+//Variavel para receber a letra na posição 0 e 8
+let letras = nome[0] + nome[8];
+
+//Variavel com nome JavaScrip
+let JavaScrip = "JavaScrip";
+
+//Variavel para receber a letra na posição 0 e 4
+let ImprimirJS = JavaScrip[0] + JavaScrip[4];
+
+//Imprimindo o resultado da Variaveis 
+console.log(letras);
+console.log(ImprimirJS);
+
+
+
+
+
+
+
+
