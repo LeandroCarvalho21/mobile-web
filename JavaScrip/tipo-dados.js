3af8dc991003304a14a8fbf1bc3087e6d0b05750
Leandro Carvalho21 <leandrocnovais@gmail.com>
2025-09-01 16:06:10 -0300

    Adicionando o arquivo tipo-dados.js

---
+++ JavaScrip/tipo-dados.js
@@ -0,0 +1,27 @@
+//Tipos de dados
+
+// string
+let nome = "Leandro Carvalho";
+
+//Interiro
+let idade = 32;
+
+
+//Booleano
+let casado = true;
+
+
+//Número Flutuante 
+let valorProduto = 5.35
+
+// Resultado
+console.log("Meu nome é: " + nome);
+// console.log("Meu nome é:",nome);
+console.log("Minha idade é:", idade);
+console.log("Casado?", casado);
+console.log("R$:", valorProduto);
+
+//para descobrir o tipo de dados usamos o operador typeof 
+console.log(typeof(idade));
+console.log(typeof(nome));
+
