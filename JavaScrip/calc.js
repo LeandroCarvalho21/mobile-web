ff496414e819e08932df54c1153ce69de64d16b2
Leandro Carvalho21 <leandrocnovais@gmail.com>
2025-09-02 14:48:54 -0300

    Adicionando exercicios de calculos com alguns operadores

---
+++ JavaScrip/calc.js
@@ -0,0 +1,18 @@
+//Duas Variaveis
+let num1 = 20;
+let num2 = 10;
+
+//Operações 
+let soma = num1 + num2; // soma
+let subtra = num1 - num2; //subitração 
+let mult = num1 * num2; //multiplicação
+let div = num1 / num2; //divisão
+
+//imprimir as respostas
+console.log("Soma: "+soma);
+console.log("Subtração: "+subtra);
+console.log("Multiplicação: "+mult);
+console.log("Divisão: "+div);
+
+
+
