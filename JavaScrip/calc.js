47da4f6aead0cc72abb3ea8359e004f75a46bffe
Leandro Carvalho21 <leandrocnovais@gmail.com>
2025-09-02 16:19:50 -0300

    criando funções de soma e subtração no JS e adicionando o eventos nos botões

--- JavaScrip/calc.js
+++ JavaScrip/calc.js
@@ -1,23 +1,68 @@
-// alert("teste");
-
 //Duas Variaveis
-let num1 = Number(prompt("Digite um número"));
-let num2 = Number(prompt("Digite outro número"));
+let num1 = 10;
+let num2 = 10;
 
-//Operações 
-let soma = num1 + num2; // soma
-let subtra = num1 - num2; //subitração 
-let mult = num1 * num2; //multiplicação
-let div = num1 / num2; //divisão
 
-//verifcar qual o tipo de dado que esta dentro dessa variavel soma
-console.log("Soma: ", typeof (soma));
+let soma = num1 + num2
+let subtra = num1 - num2
+
+
+
+//Operações
+
+// função somar
+function somar() {
+    console.log(soma);
+}
+
+// função subtrair
+function sub() {
+    console.log(subtra);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //imprimir as respostas
-console.log("Soma: ", soma);
-console.log("Subtração: ", subtra);
-console.log("Multiplicação: ", mult);
-console.log("Divisão: ", div);
+
+
+
+
+
+
+
+
+
+
+
+
+//Operações
+// let soma = num1 + num2; // soma
+// let subtra = num1 - num2; //subitração
+// let mult = num1 * num2; //multiplicação
+// let div = num1 / num2; //divisão
+
+//verifcar qual o tipo de dado que esta dentro dessa variavel soma
+// console.log("Soma: ", typeof (soma));
+
+//imprimir as respostas
+// console.log("Soma: ", soma);
+// console.log("Subtração: ", subtra);
+// console.log("Multiplicação: ", mult);
+// console.log("Divisão: ", div);
 
 /*----------------------------------------------------------------*/
 
