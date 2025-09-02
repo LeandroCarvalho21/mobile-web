012607b15bed263204cefa889f84c022a55db61c
Leandro Carvalho21 <leandrocnovais@gmail.com>
2025-09-02 15:52:36 -0300

    Adicionando o prompt para usuario digitar o número que deseja calcular

--- JavaScrip/calc.js
+++ JavaScrip/calc.js
@@ -1,8 +1,8 @@
 // alert("teste");
 
 //Duas Variaveis
-let num1 = 20;
-let num2 = 10;
+let num1 = Number(prompt("Digite um número"));
+let num2 = Number(prompt("Digite outro número"));
 
 //Operações 
 let soma = num1 + num2; // soma
@@ -10,11 +10,17 @@ let subtra = num1 - num2; //subitração
 let mult = num1 * num2; //multiplicação
 let div = num1 / num2; //divisão
 
+//verifcar qual o tipo de dado que esta dentro dessa variavel soma
+console.log("Soma: ", typeof (soma));
+
 //imprimir as respostas
-console.log("Soma: ",soma);
-console.log("Subtração: ",subtra);
-console.log("Multiplicação: ",mult);
-console.log("Divisão: ",div);
+console.log("Soma: ", soma);
+console.log("Subtração: ", subtra);
+console.log("Multiplicação: ", mult);
+console.log("Divisão: ", div);
+
+/*----------------------------------------------------------------*/
+
 
 
 
