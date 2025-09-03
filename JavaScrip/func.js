b70738629621f098b0abefc6752e0ea9baaff275
Leandro Carvalho21 <leandrocnovais@gmail.com>
2025-09-03 14:55:49 -0300

    Aprendendo usar função e exibindo o resultado no console e no html

---
+++ JavaScrip/func.js
@@ -0,0 +1,12 @@
+function mostrarNome(nome) {
+    document.write(nome);
+}
+
+function pularLinha() {
+    document.write("<br><br><br><br><br><br>")
+
+}
+
+mostrarNome("Leandro");
+pularLinha();
+mostrarNome("Dudu");
