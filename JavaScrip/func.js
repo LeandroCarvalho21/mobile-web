8a8209508b247efe247f9ee97b31429a33a08a29
Leandro Carvalho21 <leandrocnovais@gmail.com>
2025-09-03 15:35:52 -0300

    Adicionando função para pular linha e função com alguns parametros

--- JavaScrip/func.js
+++ JavaScrip/func.js
@@ -1,12 +1,13 @@
-function mostrarNome(nome) {
+function mostrarNome(nome, idade, email ) {
     document.write(nome);
+    document.write(idade);
+    document.write(email);
 }
 
 function pularLinha() {
     document.write("<br><br><br><br><br><br>")
-
 }
 
-mostrarNome("Leandro");
+mostrarNome("Leandro: ",32, " Email: leandrocnovais@gmail.com");
 pularLinha();
 mostrarNome("Dudu");
