b106bd3608b8c74459ee1ddb12864de0bca8ac29
Leandro Carvalho21 <leandrocnovais@gmail.com>
2025-09-04 16:22:04 -0300

    Adicionando funcões e operadores para calculadora

--- JavaScrip/calculadora.js
+++ JavaScrip/calculadora.js
@@ -1,5 +1,43 @@
 //variavel 
 const num1 = document.getElementById("numero1");
 const num2 = document.getElementById("numero2");
+const resultado = document.getElementById("resultado");
 
 
+//função
+function somar(event) {
+    event.preventDefault();
+
+    //exibe no console os números digitados
+    console.log(num1.value);
+    console.log(num2.value);
+
+
+    resultado.innerHTML = Number(num1.value) + Number(num2.value);
+    num1.value = ""
+    num2.value = ""
+    // document.innerHTML.resultado = num1 + num2;
+}
+
+function sub(event) {
+    event.preventDefault();
+    resultado.innerHTML = Number(num1.value) - Number(num2.value);
+    num1.value = ""
+    num2.value = ""
+}
+
+
+function mult(event) {
+    event.preventDefault();
+    resultado.innerHTML = Number(num1.value) * Number(num2.value);
+    num1.value = ""
+    num2.value = ""
+}
+
+function div(event) {
+    event.preventDefault();
+    resultado.innerHTML = Number(num1.value) / Number(num1.value);
+    num1.value = ""
+    num2.value = ""
+    
+}
