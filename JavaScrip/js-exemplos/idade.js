1f789bbc23384554015d844d66911dc4d9d0003c
Leandro Carvalho21 <leandrocnovais@gmail.com>
2025-09-05 14:51:12 -0300

    Aprendendo estrutura de decisão if else

---
+++ JavaScrip/js-exemplos/idade.js
@@ -0,0 +1,8 @@
+let idade = 18;
+
+if (idade >= 18) {
+    console.log("Pode ir pra festa")
+} else {
+    console.log("não pode ir pra festa")
+
+}
---
+++ JavaScrip/js-exemplos/nota.js
@@ -0,0 +1,10 @@
+//se a nota for >=6, o aluno esta aprovado
+// se não está reprovado
+
+let nota = 7;
+
+if (nota >= 6) {
+    console.log("Aluno aprovado");
+} else {
+    console.log("Aluno reprovado");
+}
