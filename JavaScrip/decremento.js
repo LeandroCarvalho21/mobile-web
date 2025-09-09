c33550ac37ddc0b26fbb3b244b65bfab053510ad
Leandro Carvalho21 <leandrocnovais@gmail.com>
2025-09-09 14:39:34 -0300

    Aprendendo incremento e decremento

---
+++ JavaScrip/decremento.js
@@ -0,0 +1,21 @@
+// formas de decremento 
+
+let valor = 7;
+console.log(valor);
+
+valor = valor - 1;
+console.log(`Segundo valor: ${valor}`);
+
+valor--;
+console.log(`Terceiro valor: ${valor}`);
+
+valor -= 3;
+console.log(`Quarto valor: ${valor}`);
+
+//Multiplicação
+valor *= 2 
+console.log(`Valor mutiplicicado:  ${valor}`)
+
+//Divisão
+valor /= 4;
+console.log(`Valor dividido por 4: ${valor}`);
---
+++ JavaScrip/incremento.js
@@ -0,0 +1,17 @@
+//formas de incrementar 
+let valor = 7;
+
+console.log(`Primeiro valor: ${valor}`); // valor igual a 7
+
+valor = valor + 1 //incrementa mais 1
+console.log(`Segundo valor: ${valor}`); // valor igual a 8
+
+valor++ //incrementa mais 1
+console.log(`Terceiro valor: ${valor}`); // valor igual a 9
+
+valor++ //incrementa mais 1
+console.log(`Quarto valor: ${valor}`); // valor igual a 10
+
+valor+=3 //incrementa mais 3
+console.log(`Quinto valor: ${valor}`); // valor igual a 10
+
