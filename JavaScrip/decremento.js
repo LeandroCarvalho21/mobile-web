76913dd389a69ef1cb534d89809b5f7f17fc2708
Leandro Carvalho21 <leandrocnovais@gmail.com>
2025-09-09 14:48:59 -0300

    Atualizando a descrição dos arquivos incremento e decremento

--- JavaScrip/decremento.js
+++ JavaScrip/decremento.js
@@ -1,21 +1,21 @@
 // formas de decremento 
 
 let valor = 7;
-console.log(valor);
+console.log(`Valor: ${valor}`);
 
 valor = valor - 1;
-console.log(`Segundo valor: ${valor}`);
+console.log(`Valor anterior menos 1: ${valor}`);
 
 valor--;
-console.log(`Terceiro valor: ${valor}`);
+console.log(`Valor anterior menos 1: ${valor}`);
 
 valor -= 3;
-console.log(`Quarto valor: ${valor}`);
+console.log(`Valor anterior menos 3: ${valor}`);
 
 //Multiplicação
-valor *= 2 
-console.log(`Valor mutiplicicado:  ${valor}`)
+valor *= 2
+console.log(`Valor anterior mutiplicicado por 2:  ${valor}`)
 
 //Divisão
 valor /= 4;
-console.log(`Valor dividido por 4: ${valor}`);
+console.log(`Valor anterior dividido por 4: ${valor}`);
