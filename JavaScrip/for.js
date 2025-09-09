6d67a8749fab312e15ba6708ad4e9010ea17240d
Leandro Carvalho21 <leandrocnovais@gmail.com>
2025-09-09 15:34:14 -0300

    Laço de repetição com for e template string

---
+++ JavaScrip/for.js
@@ -0,0 +1,11 @@
+//laço de repetição for com template string
+
+//laço de repetição para exibir do 0 até 5
+for (let i = 0; i <= 5; i++) {
+    console.log(`O valor de i, vale: ${i}`);
+}
+
+//laço de repetição para exibir 1 até o 10
+for (let i = 1; i <= 10; i++) {
+    console.log(`${i} Exibição`);
+}
