fc02edfda2d7c47be6fb287444b5c96098d0393b
Leandro Carvalho21 <leandrocnovais@gmail.com>
2025-09-15 16:16:49 -0300

    Adicionando logica nativa do JS para acrecentar o 0 na frente do hora, minutos e segundos

--- JavaScrip/niver.js
+++ JavaScrip/niver.js
@@ -11,17 +11,32 @@ function diasParaNiver() {
 
 function atualizarHora() {
     const agora = new Date();
-    const horaAtual = agora.getHours();
-    const minutoAtual = agora.getMinutes();
-    const segundosAtual = agora.getSeconds();
+    const horaAtual = agora.getHours().toString().padStart(2, "0");
+    const minutoAtual = agora.getMinutes().toString().padStart(2, "0");
+    const segundosAtual = agora.getSeconds().toString().padStart(2, "0");
     document.getElementById("hora").textContent = `${horaAtual}:${minutoAtual}:${segundosAtual}`
-    
-    // if (segundosAtual == 0) {
-    //     segundosAtual = `0${segundosAtual}`
-    // }
+
+}
+
+function diaMesAno() {
+    const data = new Date();
+    const dia = data.getDay();
+    const mes = data.getMonth();
+    const ano = data.getFullYear();
+    let mesNiver = mes + 1
+    let diaNiver = dia + 14
+
+
+    document.getElementById("dia").textContent = `${diaNiver}`
+    document.getElementById("mes").textContent = `${mesNiver}`
+    document.getElementById("ano").textContent = `${ano}`
+
+    // console.log(mesNiver, diaNiver, ano);
 }
 
 //chamar função
 diasParaNiver();
 setInterval(atualizarHora, 1000);
+// setInterval(diaMesAno, 1000);
 atualizarHora();
+diaMesAno();
