2b1f5705dae22045717d190344ae7d98f67c5a6a
Leandro Carvalho21 <leandrocnovais@gmail.com>
2025-09-15 15:00:28 -0300

    Adicionando codigo para ver a hora atual

--- JavaScrip/niver.js
+++ JavaScrip/niver.js
@@ -3,16 +3,25 @@ function diasParaNiver() {
     const diaSemana = hoje.getDay(2); //0=Dom, 1=Seg,...., 4=Quin
     let diasFaltando = 4 - diaSemana;
 
-
-
-
     if (diasFaltando < 0) {
         diasFaltando += 7;
-        document.getElementById("dias").textContent = diasFaltando
-        
     }
-
+    document.getElementById("dias").textContent = diasFaltando
 }
 
+function atualizarHora() {
+    const agora = new Date();
+    const horaAtual = agora.getHours();
+    const minutoAtual = agora.getMinutes();
+    const segundosAtual = agora.getSeconds();
+    document.getElementById("hora").textContent = `${horaAtual}:${minutoAtual}:${segundosAtual}`
+    
+    // if (segundosAtual == 0) {
+    //     segundosAtual = `0${segundosAtual}`
+    // }
+}
 
-diasParaNiver()
+//chamar função
+diasParaNiver();
+setInterval(atualizarHora, 1000);
+atualizarHora();
