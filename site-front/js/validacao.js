bad1f6400c35248104517db1b0ba1fa80aadf231
Leandro Carvalho21 <leandrocnovais@gmail.com>
2025-09-16 16:21:46 -0300

    Adicionando logica para validar se o usuario digito @ ou .

--- site-front/js/validacao.js
+++ site-front/js/validacao.js
@@ -1,4 +1,6 @@
 const form = document.getElementById("newForm");
+const emailCliente = document.querySelector("#emailCliente")
+
 
 
 form.addEventListener("submit", function (event) {
@@ -10,10 +12,20 @@ form.addEventListener("submit", function (event) {
     if (email === "") {
         alert("Digite o seu email");
         return false
-    } else {
-        console.log(email);
-        alert(email);
     }
+
+    //Validação simples: tem @ e .
+    if (!email.includes("@") || !email.includes(".")) {
+        alert("Email invalido. tente novamente.");
+        return false
+
+    }
+
+    
+    
+    emailCliente.textContent = email
+    emailCliente.style.display = "block";
+    // alert("Email cadastro com susseso! ✅");
 
     // Limpa o campo
     form.reset();
