ad587abe07a96a7373e7443f103b248e02dda047
Leandro Carvalho21 <leandrocnovais@gmail.com>
2025-09-18 16:21:29 -0300

    Adicionando tags no aap.jsx

--- projeto-b2/src/App.jsx
+++ projeto-b2/src/App.jsx
@@ -2,6 +2,46 @@ function App() {
 
   return (
     <>
+      <header>
+        <nav>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Sobre</a></li>
+            <li><a href="#">Contato</a></li>
+          </ul>
+          <ol>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Sobre</a></li>
+            <li><a href="#">Contato</a></li>
+          </ol>
+
+        </nav>
+      </header>
+
+      <main>
+        <section>
+          <article>
+            <h1>Tags H1 até H6</h1>
+            <h2>Leandro</h2>
+            <h3>Leandro</h3>
+            <h4>Leandro</h4>
+            <h5>Leandro</h5>
+            <h6>Leandro</h6>
+            <span>Span</span>
+            <small>Small</small>
+            <hr />
+            <div>
+              <h6>div com Paragrafos</h6>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus animi ut quos possimus ratione recusandae, dolorum maiores earum. Voluptas voluptates magnam eligendi adipisci porro aliquam laudantium animi maxime vero officia.</p>
+            </div>
+          </article>
+
+        </section>
+
+
+
+      </main>
+
 
       <h1>Olá Leandro</h1>
       <h2>Benardo</h2>
