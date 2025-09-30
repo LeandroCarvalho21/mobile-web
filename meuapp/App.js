41bc9e4b916d2df83e5ea0f6da1a7195148cf606
Leandro Carvalho21 <leandrocnovais@gmail.com>
2025-09-30 16:24:33 -0300

    Adicionando estilos no app

--- meuapp/App.js
+++ meuapp/App.js
@@ -1,39 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native';
+import Prime from './src/Telas/Prime';
+
+
+
 
 export default function App() {
   return (
     <>
-      <View style={estilos.topo}>
-
-        <Text style={estilos.tituloHeader}>teste popeto</Text>
-
-      </View>
-
       <View>
-        <Text>Leandro</Text>
+
+        <Prime />
+
+
       </View>
-
     </>
-
   );
 }
 
-const estilos = StyleSheet.create({
-  topo: {
-    width: "100%",
-    height: "150",
-    backgroundColor: "#87878771",
-  },
-
-  tituloHeader: {
-    width: "100%",
-    color: "#2d2d2dff",
-    textAlign: "center",
-    paddingTop: 60,
-    fontSize: 40,
-  },
-
-
-
-
-});
--- meuapp/src/Telas/Prime.js
+++ meuapp/src/Telas/Prime.js
@@ -2,8 +2,58 @@ import { View, StyleSheet, Text } from "react-native";
 
 export default function Prime() {
     return (
-        <View>
-            <Text>B1 Dev - Popeto</Text>
-        </View>
+        <>
+            <View style={estilos.topo}>
+
+                <Text style={estilos.tituloHeader}>Cursos SENAI</Text>
+
+            </View>
+
+            <View style={estilos.cursosContent}>
+                <Text style={estilos.textCurso}>HTML Completo</Text>
+                <Text style={estilos.textCurso}>Informatica Básica</Text>
+                <Text style={estilos.textCurso}>Excel Básico</Text>
+                <Text style={estilos.textCurso}>Excel Avançado</Text>
+            </View>
+
+        </>
     )
 }
+
+const estilos = StyleSheet.create({
+    topo: {
+        width: "100%",
+        height: "150",
+        backgroundColor: "#87878771",
+        borderBottomLeftRadius: 20,
+        borderBottomRightRadius: 20,
+
+    },
+
+    tituloHeader: {
+        width: "100%",
+        color: "#2d2d2dff",
+        textAlign: "center",
+        paddingTop: 60,
+        fontSize: 40,
+
+
+    },
+
+    cursosContent: {
+        marginTop: 20,
+        padding: 40,
+        backgroundColor: "#ff0000ff",
+    },
+
+    textCurso: {
+        color: "#fff",
+        fontSize: 20,
+        marginTop: 10,
+        backgroundColor: "rgba(98, 98, 98, 1)",
+        borderRadius: 20,
+        padding: 10,
+        textAlign: "center",
+        
+    }
+});
