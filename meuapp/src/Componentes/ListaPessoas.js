63e7b76f09b06646f3820af5d1bb22c0c4582f4f
Leandro Carvalho21 <leandrocnovais@gmail.com>
2025-10-03 16:17:03 -0300

    Adicionando estilização no componente ListaPessoas

--- meuapp/src/Componentes/ListaPessoas.js
+++ meuapp/src/Componentes/ListaPessoas.js
@@ -23,8 +23,56 @@ export default function ListaPessoas() {
     ];
 
     return (
-        <View>
-            <Text>Teste</Text>
+
+        <View style={estilos.container}>
+            <Text style={estilos.titulo}>Lista de pessoas</Text>
+
+            {pessoas.map((pessoas) => (
+                <View style={estilos.card}>
+                    <Text style={estilos.tituloPessoas}>ID: {pessoas.id}</Text>
+                    <Text style={estilos.tituloPessoas}>Nome: {pessoas.nome}</Text>
+                    <Text style={estilos.tituloPessoas}>Idade: {pessoas.idade}</Text>
+                </View>
+            ))}
+
         </View>
     )
 }
+
+
+const estilos = StyleSheet.create({
+
+    container: {
+        padding: 20,
+        margin: 16,
+    },
+
+    titulo: {
+        fontSize: 22,
+        fontWeight: "bold",
+        marginBottom: 15,
+        textAlign: "center",
+    },
+
+    card: {
+        margin: 16,
+        padding: 32,
+        backgroundColor: "#bb2a2aff",
+        borderRadius: 8,
+        color: "#fff",
+        borderWidth: 2,
+
+
+    },
+    tituloPessoas: {
+        margin: 4,
+        color: "#fff",
+        fontSize:18,
+    },
+
+
+
+
+
+
+})
