f1a40a43e60b2aedf2af39ff9a5942a0d16ecfb2
Leandro Carvalho21 <leandrocnovais@gmail.com>
2025-10-17 16:16:21 -0300

    Adicionando estilo na tela listaContatos

--- meuapp/src/Telas/Faq.js
+++ meuapp/src/Telas/Faq.js
@@ -1,69 +1,87 @@
 import { Text, View, StyleSheet } from 'react-native'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
 
 export default function Faq() {
-  return (
-    <>
-      <View style={estilos.Duvidas}>
-        <Text style={estilos.titleDuvidas}>Dúvidas Frequentes</Text>
-      </View>
+  const [useEffect, setFaq] = useState([]);
 
-      <View style={estilos.duvidasFaq}>
-        <Text style={estilos.textFaq}>Qual a metodologia?</Text>
-      </View>
+  const faq = () => {
+    axios
 
-      <View style={estilos.duvidasFaq}>
-        <Text style={estilos.textFaq}>Qual é a duração do curso?</Text>
-      </View>
+      .get("http://10.0.2.2:3000/faq")
+      .then((respostafaq) => {
+        setFaq(respostafaq.data)
+      })
+      .get("http://10.0.2.2:3000/contatos")
+      .then((resposta) => {
+        setContatos(resposta.data)
+      })
 
-      <View style={estilos.duvidasFaq}>
-        <Text style={estilos.textFaq}>Tem certificado?</Text>
-      </View>
 
-      <View style={estilos.duvidasFaq}>
-        <Text style={estilos.textFaq}>Tenho alguma garantia?</Text>
-      </View>
+    return (
+      <>
+        <View style={estilos.Duvidas}>
+          <Text style={estilos.titleDuvidas}>Dúvidas Frequentes</Text>
+        </View>
 
-      <View style={estilos.duvidasFaq}>
-        <Text style={estilos.textFaq}>Será que relamente eu consigo aprender?</Text>
-      </View>
-    </>
-  )
-}
+        <View style={estilos.duvidasFaq}>
+          <Text style={estilos.textFaq}>Qual a metodologia?</Text>
+        </View>
 
-const estilos = StyleSheet.create({
+        <View style={estilos.duvidasFaq}>
+          <Text style={estilos.textFaq}>Qual é a duração do curso?</Text>
+        </View>
 
-  titleDuvidas: {
-    textAlign: "center",
-    fontSize: 29,
-    fontWeight: "bold",
-    backgroundColor: "#9e9d9d65",
-    padding: 30,
-    borderBottomLeftRadius: 20,
-    borderTopRightRadius: 20,
-    borderWidth: 1,
-  },
+        <View style={estilos.duvidasFaq}>
+          <Text style={estilos.textFaq}>Tem certificado?</Text>
+        </View>
 
-  Duvidas: {
-    padding: 20,
-    textDecorationLine: "underline",
-  },
+        <View style={estilos.duvidasFaq}>
+          <Text style={estilos.textFaq}>Tenho alguma garantia?</Text>
+        </View>
 
-  duvidasFaq: {
-    backgroundColor: "rgba(0, 85, 34, 0.18)",
-    padding: 20,
-    marginLeft: 8,
-    marginRight: 8,
-    marginTop: 8,
-    borderRadius: 4,
-    borderColor: "rgba(0, 85, 34, 0.64)",
-    borderWidth: 1,
-    borderBottomWidth: 2,
-  },
-
-  textFaq: {
-    color: "#3e3e3eff",
-    fontSize: 17,
-    textAlign: "center",
+        <View style={estilos.duvidasFaq}>
+          <Text style={estilos.textFaq}>Será que relamente eu consigo aprender?</Text>
+        </View>
+      </>
+    )
   }
-})
+
+  const estilos = StyleSheet.create({
+
+    titleDuvidas: {
+      textAlign: "center",
+      fontSize: 29,
+      fontWeight: "bold",
+      backgroundColor: "#9e9d9d65",
+      padding: 30,
+      borderBottomLeftRadius: 20,
+      borderTopRightRadius: 20,
+      borderWidth: 1,
+    },
+
+    Duvidas: {
+      padding: 20,
+      textDecorationLine: "underline",
+    },
+
+    duvidasFaq: {
+      backgroundColor: "rgba(0, 85, 34, 0.18)",
+      padding: 20,
+      marginLeft: 8,
+      marginRight: 8,
+      marginTop: 8,
+      borderRadius: 4,
+      borderColor: "rgba(0, 85, 34, 0.64)",
+      borderWidth: 1,
+      borderBottomWidth: 2,
+    },
+
+    textFaq: {
+      color: "#3e3e3eff",
+      fontSize: 17,
+      textAlign: "center",
+    }
+  })
 
--- meuapp/src/Telas/ListaContatos.js
+++ meuapp/src/Telas/ListaContatos.js
@@ -1,33 +1,71 @@
-import { useEffect, useState } from 'react'
-import { Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react'
+import { Text, View, StyleSheet } from 'react-native'
 import axios from 'axios';
-
 
 
 export default function ListaContatos() {
     const [contatos, setContatos] = useState([]);
 
-    // Função para buscar contatos do servidor 
+    // Funçaõ para buscar contatos do servidor
     const listaContatos = () => {
         axios
             .get("http://10.0.2.2:3000/contatos")
             .then((resposta) => {
                 setContatos(resposta.data)
-
             })
             .catch((error) => {
-                console.error("Erro ao buscar contatos", error)
-                
+                console.error("Erro ao buscar contatos", error);
             });
     }
-    //Use o useEffect para buscar dados
-    <Text>Lista Contatos</Text>
+
+    // Use o useEffect para buscar dados
     useEffect(() => {
-        listaContatos()
+        listaContatos();
     }, [])
 
     return (
-        <View>
+        <View style={estilos.container}>
+            <Text style={estilos.titulo}>Contatos</Text>
+            {contatos.length > 0 ? (
+                contatos.map((contato, index) => (
+                    <View key={index} style={estilos.card}>
+                        <Text style={estilos.contato}>Nome: {contato.nome}</Text>
+                        <Text style={estilos.contato}>Telefone: {contato.telefone}</Text>
+                    </View>
+                ))
+
+            ) : (
+                <Text> Nenhum contato disponivel </Text>
+            )}
         </View>
     )
 }
+
+const estilos = StyleSheet.create({
+    titulo: {
+        fontSize: 25,
+        textAlign: "center",
+        margin: 20,
+    },
+
+    card: {
+        backgroundColor: "#b8b8b8ff",
+        padding: 4,
+        margin: 8,
+        borderWidth: 1,
+        borderColor: "#f77c00c6",
+        borderRadius: 4,
+        width: 300,
+        elevation: 10,
+    },
+
+    contato: {
+        textAlign: "center",
+    },
+
+    container: {
+        alignItems: "center",
+    }
+})
+
+
